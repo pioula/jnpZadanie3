3f2d3ad4b705308459001fa1f54c4600998c84d9
Piotr Ulanowski <pioula111@gmail.com>
2021-11-15 17:31:52 +0100

    Zamieniłem TriFuzzyRank na tuple

--- fuzzy.h
+++ fuzzy.h
@@ -1,73 +1,48 @@
 #ifndef FUZZY_H
 #define FUZZY_H
 #include <iostream>
-#include <valarray>
+#include <tuple>
+#include <cmath>
 
-typedef double real_t;
+using real_t = double;
 
 namespace rank {
-    class TriFuzzyRank {
-    public:
-        TriFuzzyRank() = default;
-        TriFuzzyRank(const TriFuzzyRank& that) = default;
-        TriFuzzyRank(TriFuzzyRank&& that) = default;
-        ~TriFuzzyRank() = default;
-        TriFuzzyRank(real_t l, real_t m, real_t u) {
-            //To wygląda jak totalny syf, ale to co robi to liczy rangę dla TriFuzzyLiczby
-            //W tej czytance jest napisane, że tak trzeba robić:
-            //https://web.archive.org/web/20160709175149/http://www.informit.com/guides/content.aspx?g=cplusplus&seqNum=198
-            x1 = u - l;
+    using rank_t = std::tuple<real_t, real_t, real_t>;
+}
 
-            real_t tmp2 = u - m;
-            tmp2 *= tmp2;
-            ++tmp2;
-            tmp2 = std::sqrt(tmp2);
+/*
+To jest kod na obliczenie rank prz zminimalizowaniu używania temporaries
+x1 = u - l;
 
-            real_t tmp3 = m - l;
-            tmp3 *= tmp3;
-            ++tmp3;
-            tmp3 = std::sqrt(tmp3);
+real_t tmp2 = u - m;
+tmp2 *= tmp2;
+++tmp2;
+tmp2 = std::sqrt(tmp2);
 
-            real_t z = x1;
-            z += tmp2;
-            z += tmp3;
+real_t tmp3 = m - l;
+tmp3 *= tmp3;
+++tmp3;
+tmp3 = std::sqrt(tmp3);
 
-            x2 = x1;
-            x2 /= z;
-            x2 *= (-1);
+real_t z = x1;
+z += tmp2;
+z += tmp3;
 
-            x1 *= m;
-            tmp2 *= l;
-            tmp3 *= u;
+x2 = x1;
+x2 /= z;
+x2 *= (-1);
 
-            x1 += tmp2;
-            x1 += tmp3;
-            x1 /= z; //x
+x1 *= m;
+tmp2 *= l;
+tmp3 *= u;
 
-            x1 += x2;
-            ++x2;
-            x3 = m;
-        }
+x1 += tmp2;
+x1 += tmp3;
+x1 /= z; //x
 
-        bool operator==(const TriFuzzyRank &other) const;
-        std::partial_ordering operator<=>(const TriFuzzyRank &other) const;
-    private:
-        real_t x1, x2, x3;
-    }; //TriFuzzyRank
-
-    bool TriFuzzyRank::operator==(const TriFuzzyRank &other) const {
-        return x1 == other.x1 &&
-                x2 == other.x2 &&
-                x3 == other.x3;
-    }
-
-    std::partial_ordering
-    TriFuzzyRank::operator<=>(const TriFuzzyRank &other) const {
-        return x1 != other.x1 ? x1 <=> other.x1 :
-            x2 != other.x2 ? x2 <=> other.x2 :
-                x3 <=> other.x3;
-    }
-}//rank
+x1 += x2;
+++x2;
+x3 = m;*/
 
 class TriFuzzyNum {
 public:
@@ -89,33 +64,32 @@ public:
         return l;
     }
 
-    TriFuzzyNum& operator=(const TriFuzzyNum& that) = default;
-    TriFuzzyNum& operator=(TriFuzzyNum&& that) = default;
+    constexpr TriFuzzyNum& operator=(const TriFuzzyNum& that) = default;
+    constexpr TriFuzzyNum& operator=(TriFuzzyNum&& that) = default;
 
-    TriFuzzyNum& operator+=(const TriFuzzyNum& that);
-    TriFuzzyNum& operator-=(const TriFuzzyNum& that);
-    TriFuzzyNum& operator*=(const TriFuzzyNum& that);
+    constexpr TriFuzzyNum& operator+=(const TriFuzzyNum& that);
+    constexpr TriFuzzyNum& operator-=(const TriFuzzyNum& that);
+    constexpr TriFuzzyNum& operator*=(const TriFuzzyNum& that);
 
-    const TriFuzzyNum operator+(const TriFuzzyNum &other) const {
+    constexpr  TriFuzzyNum operator+(const TriFuzzyNum &other) const {
         return TriFuzzyNum(*this) += other;
     }
 
-    const TriFuzzyNum operator-(const TriFuzzyNum &other) const {
+    constexpr  TriFuzzyNum operator-(const TriFuzzyNum &other) const {
         return TriFuzzyNum(*this) -= other;
     }
 
-    const TriFuzzyNum operator*(const TriFuzzyNum &other) const {
+    constexpr  TriFuzzyNum operator*(const TriFuzzyNum &other) const {
         return TriFuzzyNum(*this) *= other;
     }
 
-    bool operator==(const TriFuzzyNum &other) const { //to jest jakieś nieefektywne, ale sqrt nie jest constexpr
-        return rank::TriFuzzyRank(l, m, u) ==
-            rank::TriFuzzyRank(other.l, other.m, other.u);
+    constexpr bool operator==(const TriFuzzyNum &other) const {
+        return calculate_rank(l, m, u) == calculate_rank(other.l, other.m, other.u);
     }
 
-    std::partial_ordering operator<=>(const TriFuzzyNum &other) const {//same ^
-        return rank::TriFuzzyRank(l, m, u) <=>
-               rank::TriFuzzyRank(other.l, other.m, other.u);
+    constexpr auto operator<=>(const TriFuzzyNum &other) const {
+        return calculate_rank(l, m, u) <=>
+            calculate_rank(other.l, other.m, other.u);
     }
 
 private:
@@ -127,6 +101,18 @@ private:
     }
 
     static constexpr void order_numbers(real_t &a, real_t &b, real_t &c);
+
+    static constexpr rank::rank_t calculate_rank(const real_t _l,
+                                                 const real_t _m,
+                                                 const real_t _u) {
+        real_t z = (_u - _l) + std::sqrt(1 + (_u - _m) * (_u - _m))
+                   + std::sqrt(1 + (_m - _l) * (_m - _l));
+        real_t x = ((_u - _l) * _m + std::sqrt(1 + (_u - _m) * (_u - _m)) * _l
+                    + std::sqrt(1 + (_m - _l) * (_m - _l)) * _u) / z;
+        real_t y = (_u - _l) / z;
+
+        return std::make_tuple(x - y / 2, 1 - y, _m);
+    }
 };
 
 constexpr void TriFuzzyNum::order_numbers(real_t &a, real_t &b, real_t &c) {
@@ -146,7 +132,7 @@ constexpr TriFuzzyNum::TriFuzzyNum(real_t _l, real_t _m, real_t _u) {
     order_numbers(l, m, u);
 }
 
-TriFuzzyNum& TriFuzzyNum::operator+=(const TriFuzzyNum& that) {
+constexpr TriFuzzyNum& TriFuzzyNum::operator+=(const TriFuzzyNum& that) {
     l += that.l;
     m += that.m;
     u += that.u;
@@ -154,7 +140,7 @@ TriFuzzyNum& TriFuzzyNum::operator+=(const TriFuzzyNum& that) {
     return *this;
 }
 
-TriFuzzyNum& TriFuzzyNum::operator-=(const TriFuzzyNum& that) {
+constexpr TriFuzzyNum& TriFuzzyNum::operator-=(const TriFuzzyNum& that) {
     l -= that.u;
     m -= that.m;
     u -= that.l;
@@ -162,7 +148,7 @@ TriFuzzyNum& TriFuzzyNum::operator-=(const TriFuzzyNum& that) {
     return *this;
 }
 
-TriFuzzyNum& TriFuzzyNum::operator*=(const TriFuzzyNum& that) {
+constexpr TriFuzzyNum& TriFuzzyNum::operator*=(const TriFuzzyNum& that) {
     l *= that.l;
     m *= that.m;
     u *= that.u;
@@ -172,5 +158,7 @@ TriFuzzyNum& TriFuzzyNum::operator*=(const TriFuzzyNum& that) {
     order_numbers(l, m, u);
     return *this;
 }
+
+
 
 #endif //FUZZY_H
